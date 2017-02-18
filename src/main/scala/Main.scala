9e39af154623131dfa5fe732287113a526140bf5
Carsten Elton Sorensen <csoren@gmail.com>
2017-02-18 15:56:12 +0100

    Patch id/index is used as option value

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,8 @@
-import com.thoughtworks.binding.Binding.Vars
 import com.thoughtworks.binding.{Binding, dom}
 import droid.Patch
 import materialize._
 import org.scalajs.dom.Node
-import org.scalajs.dom.html.Select
-import org.scalajs.dom.html.{Option => HtmlOption}
+import org.scalajs.dom.html.{Select, Option => HtmlOption}
 import org.scalajs.jquery.jQuery
 import org.scalajs.{dom => jsdom}
 import rxscalajs.Subject
@@ -12,19 +10,25 @@ import rxscalajs.Subject
 import scala.concurrent.Future
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 import scala.scalajs.js
-import scala.scalajs.js.Array
 import scala.util.{Failure, Success}
 
 object Main extends js.JSApp {
-  private val patchListObservable: Subject[List[(String, Patch)]] = Subject()
+  private case class OptionValue(index: Int, patch: Patch)
 
-  private val allPatches: Future[Array[Patch]] =
-    droid.Patch.load("droidpatches.json").map(_.sortBy(_.name))
+  private val patchListObservable: Subject[List[(String, OptionValue)]] = Subject()
 
-  private def categorizedPatches: Future[Map[String, List[Patch]]] =
-    allPatches.map {
-      _.toList
-        .flatMap(patch => patch.tags.map((_, patch)))
+  private val allPatches: Future[js.Array[Patch]] =
+    droid.Patch
+      .load("droidpatches.json")
+      .map(_.sortBy(_.name))
+
+  private def categorizedPatches: Future[Map[String, List[OptionValue]]] =
+    allPatches
+      .map {
+        _.toList
+        .zipWithIndex
+        .map { case (patch, index) => OptionValue(index, patch) }
+        .flatMap(v => v.patch.tags.map((_, v)))
         .groupBy(_._1)
         .map { case (tag, list) => (tag, list.map(_._2)) }
         .toMap
@@ -33,23 +37,27 @@ object Main extends js.JSApp {
   private def updatePatches(): Unit = {
     categorizedPatches.onComplete {
       case Success(patches) =>
-        val patchNames = patches.toList.flatMap { case (tag, patchList) => patchList.map(p => (s"[$tag] ${p.name}", p)) }
+        val patchNames =
+          patches.toList
+            .sortBy(_._1)
+            .flatMap { case (tag, patchList) => patchList.map(p => (s"[$tag] ${p.patch.name}", p)) }
         patchListObservable.next(patchNames)
       case Failure(str) =>
         println(str)
     }
   }
 
-  private def patchOption(text: String, value: Patch): HtmlOption = {
+  private def patchOption(text: String, value: OptionValue): HtmlOption = {
     val option = jsdom.document.createElement("option").asInstanceOf[HtmlOption]
     option.text = text
+    option.value = value.index.toString
     option
   }
 
   @dom
   private def patchSelector: Binding[Select] = {
     val select: Select = <select />
-    patchListObservable.subscribe { (patches: List[(String, Patch)]) =>
+    patchListObservable.subscribe { (patches: List[(String, OptionValue)]) =>
       val options = patches.map { case (text, value) => patchOption(text, value) }
       jQuery(select)
         .empty()
