9101e0f780d8ebeca88727a4220505f74380b54c
Carsten Elton Sorensen <csoren@gmail.com>
2017-03-20 22:21:30 +0100

    Added playNote to Output

--- src/main/scala/MainPresenter.scala
+++ src/main/scala/MainPresenter.scala
@@ -1,4 +1,4 @@
-import midi.webmidi.{Channel, Output}
+import midi.webmidi.{Channel, InputEventName, Output}
 
 class MainPresenter(editorView: ui.Editor, midiView: ui.Midi) {
 
@@ -11,12 +11,15 @@ class MainPresenter(editorView: ui.Editor, midiView: ui.Midi) {
         (first._1, first._2, second)
       }
 
+  private val noteOn =
+    midiPresenter.selectedMidiInput
+      .map(v => v._1.observableOf(InputEventName.noteon, v._2))
+      .switch
+
   private def transmitControlChanges(midiPort: Output, channel: Channel, controlChanges: Seq[(Int, Int)]): Unit =
     controlChanges.foreach { case (control, value) => midiPort.sendControlChange(control, value, channel) }
 
   transmitPatch.subscribe { (transmitControlChanges _).tupled }
 
-  midiPresenter.selectedMidiInput
-    .
 
 }
--- src/main/scala/midi/webmidi/Output.scala
+++ src/main/scala/midi/webmidi/Output.scala
@@ -7,24 +7,33 @@ import scala.scalajs.js.|
 
 
 @ScalaJSDefined
-class Options(time: js.UndefOr[Long] = js.undefined) extends js.Object
+class ControlChangeOptions(time: js.UndefOr[Long] = js.undefined) extends js.Object
+
+
+@ScalaJSDefined
+class PlayNoteOptions(duration: js.UndefOr[Long] = js.undefined, rawVelocity: Boolean = false, release: Float = 0.5f, time: js.UndefOr[Long] = js.undefined, velocity: Float | Int = 0.5f) extends js.Object
 
 
 @js.native
 @JSName("Output")
 class Output extends Port {
   @JSName("sendControlChange")
-  private[webmidi] def _sendControlChange(controller: Int | String, value: js.UndefOr[Int] = js.undefined, channel: js.UndefOr[Int | String] = js.undefined, options: js.UndefOr[Options] = js.undefined): Output = js.native
+  private[webmidi] def _sendControlChange(controller: Int | String, value: js.UndefOr[Int] = js.undefined, channel: js.UndefOr[Int | String] = js.undefined, options: js.UndefOr[ControlChangeOptions] = js.undefined): Output = js.native
+
+  @JSName("playNote")
+  private[webmidi] def _playNote(note: Int | String | Array[Int], channel: js.UndefOr[Int | String] = js.undefined, options: js.UndefOr[PlayNoteOptions] = js.undefined): Output = js.native
 }
 
 
 object Output {
-  private def asJs(time: Option[Long]): js.UndefOr[Options] =
-    time.fold[js.UndefOr[Options]](js.undefined)(t => js.defined(new Options(time = t)))
+  private def asJs(time: Option[Long]): js.UndefOr[ControlChangeOptions] =
+    time.fold[js.UndefOr[ControlChangeOptions]](js.undefined)(t => js.defined(new ControlChangeOptions(time = t)))
 
   implicit final class OutputExt(val output: Output) {
-    def sendControlChange(controller: Int, value: Int = 0, channel: Channel = All(), time: Option[Long] = None): Output = {
+    def sendControlChange(controller: Int, value: Int = 0, channel: Channel = All(), time: Option[Long] = None): Output =
       output._sendControlChange(controller, value, channel.asJs, asJs(time))
-    }
+
+    def playNote(note: Int, channel: Channel = All(), velocity: Int = 96): Output =
+      output._playNote(note, channel.asJs, new PlayNoteOptions(rawVelocity = true, velocity = velocity))
   }
 }
