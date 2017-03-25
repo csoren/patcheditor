68e1f75444c87ef0b00e9fc8274b4e88f9f885a4
Carsten Elton Sorensen <csoren@gmail.com>
2017-03-25 16:16:09 +0100

    Added patches to docs

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 cp -v index-fastopt.html docs/index.html
 cp -v target/scala-2.12/patcheditor-jsdeps.min.js docs
 cp -v target/scala-2.12/patcheditor-opt.js docs
+cp -v droidpatches.json docs
 
 cd docs
 sed -i -- 's|./target/scala-2.12/patcheditor-jsdeps.js|./patcheditor-jsdeps.min.js|g' index.html
