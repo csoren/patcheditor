45998e25d385662744bdec880d08beb07c84d7aa
Carsten Elton Sorensen <csoren@gmail.com>
2017-03-25 16:14:45 +0100

    Added docs folder

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cp -v index-fastopt.html docs/index.html
+cp -v target/scala-2.12/patcheditor-jsdeps.min.js docs
+cp -v target/scala-2.12/patcheditor-opt.js docs
+
+cd docs
+sed -i -- 's|./target/scala-2.12/patcheditor-jsdeps.js|./patcheditor-jsdeps.min.js|g' index.html
+sed -i -- 's|./target/scala-2.12/patcheditor-fastopt.js|./patcheditor-opt.js|g' index.html
+
