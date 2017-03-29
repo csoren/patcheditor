b0db42492bb87d0c1771f4df97bcffae42846914
Carsten Elton Sorensen <csoren@gmail.com>
2017-03-29 13:49:17 +0200

    Build and deploy fixes

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-cp -v index-fastopt.html docs/index.html
-cp -v target/scala-2.12/patcheditor-jsdeps.min.js docs
-cp -v target/scala-2.12/patcheditor-opt.js docs
-cp -v droidpatches.json docs
+sbt fullOptJS
+polymer build
+cd build/default
+sed -i -- 's|./target/scala-2.12/patcheditor-jsdeps.js|./target/scala-2.12/patcheditor-jsdeps.min.js|g' index.html
+sed -i -- 's|./target/scala-2.12/patcheditor-fastopt.js|./target/scala-2.12/patcheditor-opt.js|g' index.html
 
-cd docs
-sed -i -- 's|./target/scala-2.12/patcheditor-jsdeps.js|./patcheditor-jsdeps.min.js|g' index.html
-sed -i -- 's|./target/scala-2.12/patcheditor-fastopt.js|./patcheditor-opt.js|g' index.html
-
+cd ../..
+rm -rf docs/*
+cp -rv build/default/* docs
